24d95dd238d80dac3502353678e7943260eb873b
Phong Tran <phongtran11.tt@gmail.com>
2021-10-09 19:52:16 +0700

    feat: init router login

--- server/router/auth.js
+++ server/router/auth.js
@@ -37,15 +37,77 @@ router.post('/register', async (req, res) => {
         await newuser.save();
 
         // Return token
-        const accessToken = jwt.sign({userId: newuser._id}, process.env.ACCESS_TOKEN);
+        const accessToken = jwt.sign({
+            userId: newuser._id},
+            process.env.ACCESS_TOKEN);
 
-        res.json({Success: true, message:'User created successfully', accessToken});
+        res.json({
+            Success: true,
+            message:'User created successfully',
+            accessToken
+        });
     } 
     catch(err) {
         console.log(err);
-        res.status(500).json({sussces: false, message: 'Interal server error'});
+        res.status(500).json({
+            sussces: false,
+            message: 'Interal server error'
+        });
     }
 }); 
 
+
+// POST /api/auth/login
+router.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+
+    // Validation
+    if(!username || !password) {
+        return res.status(400, {
+            success: false,
+            message: 'Missing username or password',
+        });
+    };
+
+    try {
+        const user = await User.findOne({username});
+        
+        //  Verify username
+        if(!user) {
+            return res.status(400).json({
+                success: false,
+                message: 'Incorrect username or password!!!'
+            });
+        }
+
+        //  Verify password
+        const passwordValid = await argon2.verify(user.password, password);
+
+        if (!passwordValid) {
+            return res.status(400).json({
+                success: false,
+                message: 'Incorrect username or password!!!'
+            });
+        }
+
+        // Return token
+        const accessToken = jwt.sign({
+            userId: user._id},
+            process.env.ACCESS_TOKEN);
+
+        res.json({
+            Success: true,
+            message:'Login successfully',
+            accessToken
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            sussces: false,
+            message: 'Interal server error'
+        });
+    }
+})
 
 module.exports = router;
